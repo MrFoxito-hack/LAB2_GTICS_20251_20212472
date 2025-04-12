16e0678dce38b93233e60b373dcc332b6a0f7f57
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-04-11 20:20:35 -0500

    commit prueba

--- src/main/java/com/example/lab2_20212472/Lab220212472Application.java
+++ src/main/java/com/example/lab2_20212472/Lab220212472Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab220212472Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab220212472Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab220212472Application.class, args);
+    }
 
 }
