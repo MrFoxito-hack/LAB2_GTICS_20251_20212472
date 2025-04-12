dc0d774f888e351b1411abaf8c56d4698b07b7b2
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-04-11 20:09:17 -0500

    first commit

---
+++ src/test/java/com/example/lab2_20212472/Lab220212472ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab2_20212472;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab220212472ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
