045318a52717f7e5eb18b018559ee4b05574ad46
MrFoxito-hack <miguelalvizuri7@gmail.com>
2025-04-11 21:33:29 -0500

    agregamos los formularios y cambiamos un poco el controlador

--- src/main/java/com/example/lab2_20212472/controlador/AutoControlador.java
+++ src/main/java/com/example/lab2_20212472/controlador/AutoControlador.java
@@ -6,8 +6,10 @@ import com.example.lab2_20212472.repositorio.AutoRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AutoControlador {
@@ -24,5 +26,19 @@ public class AutoControlador {
         List<Auto> lista = autoRepositorio.findAll();
         model.addAttribute("listaAutos", lista);
         return "autos";
+    }
+
+    //mostramos el formulario para crear un nuevo auto
+    @GetMapping("/auto/nuevo")
+    public String mostrarFormularioNuevoAuto(Model model) {
+        model.addAttribute("auto", new Auto());
+        return "autoFormulario";
+    }
+
+    //guardamos el nuevo auto
+    @PostMapping("/auto/guardar")
+    public String guardarAuto(@ModelAttribute("auto") Auto auto) {
+        autoRepositorio.save(auto);
+        return "redirect:/autos";
     }
 }
--- src/main/java/com/example/lab2_20212472/controlador/SeguroControlador.java
+++ src/main/java/com/example/lab2_20212472/controlador/SeguroControlador.java
@@ -1,13 +1,17 @@
 //Sabemos que para utilizar los repositorios de los controladores, estos
 //deben ser definidos como atributos finales de la clase e iniciarlos en el constructor
 package com.example.lab2_20212472.controlador;
+
 import com.example.lab2_20212472.modelo.Seguro;
 import com.example.lab2_20212472.repositorio.SeguroRepositorio;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.List;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SeguroControlador {
@@ -27,5 +31,19 @@ public class SeguroControlador {
         List<Seguro> lista = seguroRepositorio.findAll();
         model.addAttribute("listaSeguros", lista);
         return "seguros"; // retorna la vista seguros.html
+    }
+
+    //mostrar el formulario para crear un nuevo seguro
+    @GetMapping("/seguro/nuevo")
+    public String mostrarFormularioNuevoSeguro(Model model) {
+        model.addAttribute("seguro", new Seguro());
+        return "seguroFormulario";
+    }
+
+    //guardar el nuevo seguro
+    @PostMapping("/seguro/guardar")
+    public String guardarSeguro(@ModelAttribute("seguro") Seguro seguro) {
+        seguroRepositorio.save(seguro);
+        return "redirect:/seguros";
     }
 }
